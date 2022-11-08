532761db0a398bc89d7ffb380fea5e829490722b
Andrea Vasapollo <andrea.vasapollo@checkout.com>
2022-11-08 16:37:04 +0000

    chore: 01-client

--- 01-client/main.go
+++ 01-client/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"context"
 
+	"github.com/avasapollo/eth-playground/config"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	c := config.Get()
+
+	lgr := logrus.New()
+
+	client, err := ethclient.DialContext(context.Background(), c.NetworkURL)
+	if err != nil {
+		lgr.WithError(err).Fatalf("can't get the client")
+	}
+	defer client.Close()
+
+	// get the last block of the chain
+	block, err := client.BlockByNumber(context.Background(), nil)
+	if err != nil {
+		lgr.WithError(err).Error("can't get the last block")
+	}
+	lgr.WithFields(logrus.Fields{
+		"block_number":  block.Number(),
+		"block_receipt": block.Header().ReceiptHash.String(),
+	}).Info("last block")
 }
--- config/config.go
+++ config/config.go
@@ -1,1 +1,15 @@
 package config
+
+import (
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	NetworkURL string `envconfig:"NETWORK_URL"`
+}
+
+func Get() *Config {
+	c := new(Config)
+	_ = envconfig.Process("", c)
+	return c
+}
