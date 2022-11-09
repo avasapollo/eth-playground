1a2ae355b94f5168292f260d43013d39e9509859
Andrea Vasapollo <andrea.vasapollo@checkout.com>
2022-11-09 15:42:18 +0000

    chore: balance and tools

--- 02-balance/main.go
+++ 02-balance/main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"math"
-	"math/big"
 
 	"github.com/avasapollo/eth-playground/config"
 	"github.com/avasapollo/eth-playground/tools"
@@ -26,17 +23,12 @@ func main() {
 
 	// get the last block of the chain
 	account := common.HexToAddress(c.MyAccount)
-	balanceAt, err := client.BalanceAt(context.Background(), account, nil)
+	balance, err := client.BalanceAt(context.Background(), account, nil)
 	if err != nil {
-		lgr.WithError(err).Error("can't get the balance of the last block")
+		lgr.WithError(err).Fatal("can't get the balance of the last block")
 	}
-	print(tools.WeiToEther(balanceAt))
-	fbalance := new(big.Float)
-	fbalance.SetString(balanceAt.String())
-	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
-	fmt.Println(ethValue)
-	b := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
-	lgr.WithFields(logrus.Fields{
-		"balance": b,
-	}).Info("balance at last block")
+	// 1 ether = 10^18 wei
+
+	valueEth, _ := tools.FromWei(tools.Ether, balance)
+	lgr.WithField("balance", valueEth).Info("balance")
 }
--- tools/tools.go
+++ tools/tools.go
@@ -1,15 +1,20 @@
 package tools
 
 import (
+	"errors"
+	"math"
 	"math/big"
-
-	"github.com/ethereum/go-ethereum/params"
 )
 
-func EtherToWei(val *big.Int) *big.Int {
-	return new(big.Int).Mul(val, big.NewInt(params.Ether))
+const Ether = "ether"
+
+func FromWei(unit string, val *big.Int) (*big.Float, error) {
+	if unit != Ether {
+		return nil, errors.New("unit is not allowed")
+	}
+	// 1 ether = 10^18 wei
+	fBalance := new(big.Float)
+	fBalance.SetString(val.String())
+	return new(big.Float).Quo(fBalance, big.NewFloat(math.Pow10(18))), nil
 }
 
-func WeiToEther(val *big.Int) *big.Int {
-	return new(big.Int).Div(val, big.NewInt(params.Ether))
-}
